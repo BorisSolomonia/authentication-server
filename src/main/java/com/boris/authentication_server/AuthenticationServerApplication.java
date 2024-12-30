e0afff2822c9353b1f4aa709b44885edf5af5d36
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2024-12-30 16:08:53 +0400

    add გამარჯობა

--- src/main/java/com/boris/authentication_server/AuthenticationServerApplication.java
+++ src/main/java/com/boris/authentication_server/AuthenticationServerApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 public class AuthenticationServerApplication {
 
+	System.out.print("გამარჯობა ბორისი");
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
